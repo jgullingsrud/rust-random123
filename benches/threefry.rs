e8a2495dc144a3ef2fe8132e11c88565038cb75d
Justin Gullingsrud <justinrocks@gmail.com>
2018-12-01 07:44:40 -0500

    Add a chacha benchmark for comparison.

--- benches/threefry.rs
+++ benches/threefry.rs
@@ -2,9 +2,13 @@
 #[macro_use]
 extern crate criterion;
 extern crate rand_threefry;
+extern crate rand_chacha;
+extern crate rand_core;
 
 use criterion::Criterion;
 use rand_threefry::{Array2x64, generate};
+use rand_chacha::ChaChaRng;
+use rand_core::{SeedableRng, RngCore};
 
 const EXAMPLE_SEED1_U64: u64 = 0xdeadbeef12345678;
 const EXAMPLE_SEED2_U64: u64 = 0xdecafbadbeadfeed;
@@ -15,5 +19,11 @@ fn bench_generate(c: &mut Criterion) {
     c.bench_function("generate", |b| b.iter(|| generate(ctr, key)));
 }
 
-criterion_group!(benches, bench_generate);
+fn bench_chacha(c: &mut Criterion) {
+    let seed = [0u8; 32];
+    let mut rng = ChaChaRng::from_seed(seed);
+    c.bench_function("chacha_next_u64", |b| b.iter(|| rng.next_u64()));
+}
+
+criterion_group!(benches, bench_generate, bench_chacha);
 criterion_main!(benches);
