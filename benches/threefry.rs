19a8386b9c1968b2b831c017e37217b7ed58cc0e
Justin Gullingsrud <justinrocks@gmail.com>
2018-12-01 11:53:08 -0500

    Implement RngCore for threefry.

--- benches/threefry.rs
+++ benches/threefry.rs
@@ -6,7 +6,7 @@ extern crate rand_chacha;
 extern crate rand_core;
 
 use criterion::Criterion;
-use rand_threefry::{Array2x64, generate};
+use rand_threefry::{ThreeFryRng,Array2x64, generate};
 use rand_chacha::ChaChaRng;
 use rand_core::{SeedableRng, RngCore};
 
@@ -16,14 +16,21 @@ const EXAMPLE_SEED2_U64: u64 = 0xdecafbadbeadfeed;
 fn bench_generate(c: &mut Criterion) {
     let ctr: Array2x64 = [0,1973];
     let key: Array2x64 = [EXAMPLE_SEED1_U64, EXAMPLE_SEED2_U64];
-    c.bench_function("generate", |b| b.iter(|| generate(ctr, key)));
+    c.bench_function("threefry_generate", |b| b.iter(|| generate(ctr, key)));
 }
 
-fn bench_chacha(c: &mut Criterion) {
+fn bench_u64(c: &mut Criterion) {
+    let ctr: Array2x64 = [0,1973];
+    let key: Array2x64 = [EXAMPLE_SEED1_U64, EXAMPLE_SEED2_U64];
+    let mut rng = ThreeFryRng { ctr, key };
+    c.bench_function("threefry_next_u64", |b| b.iter(|| rng.next_u64()));
+}
+
+fn bench_chacha_u64(c: &mut Criterion) {
     let seed = [0u8; 32];
     let mut rng = ChaChaRng::from_seed(seed);
     c.bench_function("chacha_next_u64", |b| b.iter(|| rng.next_u64()));
 }
 
-criterion_group!(benches, bench_generate, bench_chacha);
+criterion_group!(benches, bench_generate, bench_u64, bench_chacha_u64);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,35 @@
-//extern crate rand_core;
+extern crate rand_core;
 
-//use rand_core::{RngCore, SeedableRng};
+use rand_core::{RngCore, Error, impls};
 
 pub type Array2x64 = [u64; 2];
+
+pub struct ThreeFryRng {
+    pub ctr: Array2x64,
+    pub key: Array2x64,
+    // TODO: implement and benchmark a buffer so we don't
+    // burn half our bits.
+}
+
+impl RngCore for ThreeFryRng {
+    fn next_u64(&mut self) -> u64 {
+        let rand: u64 = generate(self.ctr, self.key)[0];
+        self.ctr[0] = self.ctr[0].wrapping_add(1);
+        if self.ctr[0] == 0 {
+            self.ctr[1] = self.ctr[1].wrapping_add(1);
+        }
+        rand
+    }
+    fn next_u32(&mut self) -> u32 {
+        self.next_u64() as u32
+    }
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        impls::fill_bytes_via_next(self, dest)
+    }
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        Ok(self.fill_bytes(dest))
+    }
+}
 
 // TODO: use std::fmt; impl fmt::Debug
 
@@ -76,24 +103,26 @@ pub fn generate(ctr: Array2x64, key: Array2x64) -> Array2x64 {
 
 #[cfg(test)]
 mod tests {
-    use super::{Array2x64, generate};
+    const TEST_VEC_1: [u64; 20] = [
+        0x3c956fe5e3e09745,  0x911f953cce0c0674,
+        0xbf307d9a09b8e517,  0x21255fa6b494c50e,
+        0x36767323a0f90211,  0xb5912b450fc89b38,
+        0x2d5703abf89c5424,  0xa0c7471ae60d0622,
+        0x24a9f70a44338b6d,  0xd1396ce94674b224,
+        0xa30049ea40bfed1 ,  0xec7400474a7fe8f0,
+        0x37fa17b7c8b37514,  0x7d38e5e4f0eb3a1,
+        0x1e176ae521a2c8c6,  0x88f7022bae92e50d,
+        0x19ce7fbd095eb0f8,  0x65eaf3fc558b735c,
+        0xfad725f62c08e780,  0x1e91764c67bc64e6,
+    ];
+    const EXAMPLE_SEED1_U64: u64 = 0xdeadbeef12345678;
+    const EXAMPLE_SEED2_U64: u64 = 0xdecafbadbeadfeed;
+
+    use super::{ThreeFryRng, Array2x64, generate};
+    use rand_core::RngCore;
+
     #[test]
     fn exact_values() {
-        let _want: Vec<u64> = vec![
-            0x3c956fe5e3e09745,  0x911f953cce0c0674,
-            0xbf307d9a09b8e517,  0x21255fa6b494c50e,
-            0x36767323a0f90211,  0xb5912b450fc89b38,
-            0x2d5703abf89c5424,  0xa0c7471ae60d0622,
-            0x24a9f70a44338b6d,  0xd1396ce94674b224,
-            0xa30049ea40bfed1 ,  0xec7400474a7fe8f0,
-            0x37fa17b7c8b37514,  0x7d38e5e4f0eb3a1,
-            0x1e176ae521a2c8c6,  0x88f7022bae92e50d,
-            0x19ce7fbd095eb0f8,  0x65eaf3fc558b735c,
-            0xfad725f62c08e780,  0x1e91764c67bc64e6,
-        ];
-        const EXAMPLE_SEED1_U64: u64 = 0xdeadbeef12345678;
-        const EXAMPLE_SEED2_U64: u64 = 0xdecafbadbeadfeed;
-
         let mut ctr: Array2x64 = [0,0];
         let key: Array2x64 = [EXAMPLE_SEED1_U64, EXAMPLE_SEED2_U64];
         for i in 0..10 {
@@ -101,8 +130,18 @@ mod tests {
             let rand = generate(ctr, key);
             let i0 = (2*i+0) as usize;
             let i1 = (2*i+1) as usize;
-            assert_eq!(rand[0], _want[i0]);
-            assert_eq!(rand[1], _want[i1]);
+            assert_eq!(rand[0], TEST_VEC_1[i0]);
+            assert_eq!(rand[1], TEST_VEC_1[i1]);
+        }
+    }
+
+    #[test]
+    fn next_u64() {
+        let ctr: Array2x64 = [0,0];
+        let key: Array2x64 = [EXAMPLE_SEED1_U64, EXAMPLE_SEED2_U64];
+        let mut rng = ThreeFryRng { ctr, key };
+        for i in 0..10 {
+            assert_eq!(rng.next_u64(), TEST_VEC_1[2*i]);
         }
     }
 }
